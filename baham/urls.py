06cfacb47e661e6ccb1c1d99ce59739a9129cd64
Owais Ahmed Hussain <owais.a.hussain@gmail.com>
2023-02-14 01:23:33 +0500

    - Moved scritps to header.html - Adding table to populate data in vehicles.html - Adding urls for Vehicles operations - Adding createvehicle.html to create a new Vehicle object

--- baham/urls.py
+++ baham/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.view_index, name='home'),
     path('baham/members', views.view_members, name='members'),
     path('baham/vehicles', views.view_vehicles, name='vehicles'),
+    path('baham/vehicles/create', views.create_vehicle, name='create_vehicle'),
+    path('baham/vehicles/create/save/', views.save_vehicle, name='save_vehicle'),
     path('baham/aboutus', views.view_aboutus, name='aboutus')
 ]
--- baham/views.py
+++ baham/views.py
@@ -1,13 +1,21 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+
+from baham.enum_types import VehicleStatus
+from baham.models import Vehicle, VehicleModel, UserProfile
 
 
 # Create your views here.
 def view_index(request):
     template = loader.get_template('home.html')
+    # Fetch the last 20 records
+    vehicles = Vehicle.objects.all().order_by('-vehicle_id').values()
     context = {
-        "navbar": "home"
+        "navbar": "home",
+        "vehicles": vehicles
     }
     return HttpResponse(template.render(context, request))
 
@@ -26,6 +34,35 @@ def view_vehicles(request):
         "navbar": "vehicles"
     }
     return HttpResponse(template.render(context, request))
+
+
+def create_vehicle(request):
+    template = loader.get_template('createvehicle.html')
+    models = VehicleModel.objects.all().values_list('model_id', 'vendor', 'model', 'type')
+    users = User.objects.filter(is_superuser=False, is_active=True).all().values_list('id', 'first_name', 'last_name', 'email')
+    context = {
+        "navbar": "vehicles",
+        "models": models,
+        "users": users,
+        "statuses": [e.value for e in VehicleStatus]
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def save_vehicle(request):
+    registration_number = request.POST['registration_number']
+    color = request.POST['color']
+    model_id = request.POST['model_id']
+    owner_id = request.POST['owner_id']
+    status = request.POST['status']
+    if not registration_number or not model_id or not owner_id:
+        return HttpResponse('<h3 class="danger">Error! Required parameters are missing.<h3>')
+    model = VehicleModel.objects.filter(pk=model_id).get()
+    owner = UserProfile.objects.filter(pk=owner_id).get()
+    obj = Vehicle(registration_number=registration_number, color=color, model=model,
+                  owner=owner, status=status)
+    obj.save()
+    return HttpResponseRedirect(reverse('vehicles.html'))
 
 
 def view_aboutus(request):
